7511f116d654555b9472127b792de6c313d99114
Yahan Liu <yli431@uclive.ac.nz>
2024-02-25 20:25:02 +1300

    Add more admin pages and functionalities

--- pipeline_configuration/admin.py
+++ pipeline_configuration/admin.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.contrib import admin
-import django_ace
-
+import django_ace  # type: ignore
+from django.contrib import messages
 from django.contrib import admin
-from pipeline_configuration.models import Workflow, Version, PipelineRun, PipelineYaml
+from pipeline_configuration.models import PipelineExecutionRecord, Workflow, Version, PipelineRun, PipelineYaml, SpecYaml
 
 
 @admin.register(Workflow)
@@ -16,7 +16,7 @@ class VersionAdmin(admin.ModelAdmin):
     list_display = ("number",)
 
 
-class YamlAdminField(forms.ModelForm):
+class YamlBodyField(forms.ModelForm):
     class Meta:
         model = PipelineYaml
         widgets = {
@@ -25,12 +25,70 @@ class YamlAdminField(forms.ModelForm):
         fields = "__all__"
 
 
+class SpecYamlsInline(admin.StackedInline):
+    model = PipelineYaml.specyamls.through
+    extra = 0
+    can_delete = False
+    can_add = False
+    verbose_name = "Spec yaml"
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 @admin.register(PipelineYaml)
 class PipelineYamlAdmin(admin.ModelAdmin):
     list_display = ("name", "description")
-    form = YamlAdminField
+    form = YamlBodyField
+    fields = ["name", "description", "body"]
+    inlines = [SpecYamlsInline,]
+    actions = ["validate_input_output_matching", "validate_models_loading_by_name", "run_pipeline"]
+    
+    @admin.action(description="Validate modules input and output matching")
+    def validate_input_output_matching(self, request, queryset):
+        self.message_user(
+            request,
+            "All later inputs can be satified by earlier outputs successfully.",
+            messages.SUCCESS,
+        )
+        
+    @admin.action(description="Validate all models can be loaded by name.")
+    def validate_models_loading_by_name(self, request, queryset):
+        self.message_user(
+            request,
+            "All models can be loaded by name successfully.",
+            messages.SUCCESS,
+        )
+
+
+@admin.register(SpecYaml)
+class SpecYamlAdmin(admin.ModelAdmin):
+    list_display = ("name", "description")
+    form = YamlBodyField
+
 
 @admin.register(PipelineRun)
 class PipelineRunAdmin(admin.ModelAdmin):
     list_display = ("pipeline_yaml", "version", "workflow")
+    actions = ["run_pipeline"]
+    
+    @admin.action(description="Run pipeline")
+    def run_pipeline(self, request, queryset):
+        for pipeline_run in queryset:
+            PipelineExecutionRecord.objects.create(
+                status="COMPLETED",
+                pipeline_yaml=pipeline_run.pipeline_yaml,
+                pipeline_run=pipeline_run,
+            )
+            self.message_user(
+                request,
+                "Pipeline has been executed successfully.",
+                messages.SUCCESS,
+            )
 
+
+@admin.register(PipelineExecutionRecord)
+class PipelineExecutionRecordAdmin(admin.ModelAdmin):
+    list_display = ("triggered_at", "finished_at", "status")
+    fields = ("triggered_at", "finished_at", "status", "pipeline_run", "pipeline_yaml")
+    readonly_fields = ("triggered_at", "finished_at")
--- pipeline_configuration/models.py
+++ pipeline_configuration/models.py
@@ -1,4 +1,6 @@
+from enum import Enum
 from django.db import models
+
 
 class Workflow(models.Model):
     name = models.CharField(max_length=50, unique=True)
@@ -13,7 +15,7 @@ class Version(models.Model):
     def __str__(self):
         return str(self.number)
     
-    
+ 
 class PipelineYaml(models.Model):
     name = models.CharField(max_length=50, unique=True, blank=False, null=False)
     description = models.CharField(max_length=200, blank=True)
@@ -21,9 +23,40 @@ class PipelineYaml(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class SpecYaml(models.Model):
+    name = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    description = models.CharField(max_length=200, blank=True)
+    pipeline_yaml = models.ManyToManyField(PipelineYaml, related_name='specyamls')
+    body = models.TextField(blank=False, null=False)
     
+    def __str__(self):
+        return self.name
+
 
 class PipelineRun(models.Model):
     pipeline_yaml = models.ForeignKey(PipelineYaml, null=False, on_delete=models.CASCADE)
     version = models.ForeignKey(Version, null=False, on_delete=models.CASCADE)
     workflow = models.ForeignKey(Workflow, null=False, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Run for workflow[{self.workflow}] and version[{self.version}]"
+
+
+class PipelineExecutionRecord(models.Model):
+    class PipelineExecutionStatus(models.TextChoices):
+        PENDING = "PENDING", "PENDING"
+        COMPLETED = "COMPLETED", "COMPLETED"
+        FAILED = "FAILED", "FAILED"
+        ERROR = "ERROR", "ERROR"
+        
+    triggered_at = models.DateTimeField(auto_now=True)
+    finished_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=255, choices=PipelineExecutionStatus.choices, default=PipelineExecutionStatus.COMPLETED)
+    pipeline_run = models.ForeignKey(PipelineRun, null=True, blank=False, on_delete=models.SET_NULL)
+    pipeline_yaml = models.ForeignKey(PipelineYaml, null=True, blank=False, on_delete=models.SET_NULL)
+    logs = models.FileField(null=True)
+    
+    def __str__(self):
+        return str(self.pipeline_yaml.name + " triggered at " + str(self.triggered_at))
