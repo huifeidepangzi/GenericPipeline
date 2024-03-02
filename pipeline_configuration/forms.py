9728bba2e5167bf04f6b70c4563c344842fe997f
Yahan Liu <yli431@uclive.ac.nz>
2024-03-02 21:50:12 +1300

    Update pipeline yaml validation logic

--- pipeline_configuration/forms.py
+++ pipeline_configuration/forms.py
@@ -13,11 +13,44 @@ class PipelineYamlForm(forms.ModelForm):
         ]
 
     def clean(self):
+        validation_errors = []
         yaml_dict = yaml.load(self.cleaned_data["body"], Loader=SafeLoader)
-        if yaml_dict["stages"][1]["spec"] != "ado.2":
-            raise forms.ValidationError(
-                f"Bad ado number: {yaml_dict['stages'][1]['spec']}"
+        if "name" not in yaml_dict:
+            validation_errors.append(
+                forms.ValidationError("Name field is missing.", code="MISSED_NAME")
             )
+
+        if "stages" not in yaml_dict:
+            validation_errors.append(
+                forms.ValidationError("Specs field is missing.", code="MISSED_SPECS")
+            )
+        else:
+            for index, stage in enumerate(yaml_dict["stages"]):
+                if "desc" not in stage:
+                    validation_errors.append(
+                        forms.ValidationError(
+                            f"Desc field is missing in {index + 1} spec.",
+                            code="MISSED_DESC",
+                        )
+                    )
+                if "spec" not in stage:
+                    validation_errors.append(
+                        forms.ValidationError(
+                            f"Spec field is missing in {index + 1} spec.",
+                            code="MISSED_SPEC",
+                        )
+                    )
+                if "tags" not in stage:
+                    validation_errors.append(
+                        forms.ValidationError(
+                            f"Tags field is missing in {index + 1} spec.",
+                            code="MISSED_TAGS",
+                        )
+                    )
+
+        if validation_errors:
+            raise forms.ValidationError(validation_errors)
+
         return self.cleaned_data
 
     class Meta:
