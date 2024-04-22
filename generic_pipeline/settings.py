0be6b4c5f4082f4b40a89b2a91478f051b32139e
Yahan Liu <yli431@uclive.ac.nz>
2024-04-22 22:01:57 +1200

    Add login check for the add pipeline page

--- generic_pipeline/settings.py
+++ generic_pipeline/settings.py
@@ -164,7 +164,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
-        # 'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+LOGIN_URL = '/admin/login/'
