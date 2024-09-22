3bcdb152d24053b5466915f94d9e4aad332df6e2
yi shan <yi.shan@octoenergy.com>
2024-09-23 11:03:35 +1200

    Update settings.py

--- generic_pipeline/settings.py
+++ generic_pipeline/settings.py
@@ -29,7 +29,7 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "*",
     "127.0.0.1",
-    "ec2-54-252-225-114.ap-southeast-2.compute.amazonaws.com",
+    "ec2-54-252-75-49.ap-southeast-2.compute.amazonaws.com",
     "0.0.0.0",
     "localhost",
 ]
